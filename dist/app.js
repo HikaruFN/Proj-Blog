8c4f1e4ef86616029414a4c26e05526fbc55b87d
Francesco Nisi <francesconisi95@gmail.com>
2021-05-12 13:34:36 +0200

    svolgimento top footer

--- dist/app.js
+++ dist/app.js
@@ -15,7 +15,12 @@ var app = new Vue({
     topHeaderNav: ['SIGN IN', 'ABOUT US', 'CONTACT US', 'BUY NOW'],
     headerLogo: 'img/logo-default-slim.png',
     //Header Nav
-    headerNav: ['HOME', 'ELEMENTS', 'FEATURES', 'PAGES', 'PORTFOLIO', 'BLOG', 'SHOP']
+    headerNav: ['HOME', 'ELEMENTS', 'FEATURES', 'PAGES', 'PORTFOLIO', 'BLOG', 'SHOP'],
+    //Top Footer
+    //Bottom Footer
+    footerLogo: 'img/logo-footer.png',
+    copyright: '© Copyright 2020. All Rights Reserved',
+    bottomFooterLinks: ['FAQ', 'Site map', 'Contact Us']
   }
 });
 
